472af6ed5a2c99ed5ccf4c71a19c556070905e5a
sakshithorat13 <143682266+sakshithorat13@users.noreply.github.com>
2026-06-10 09:34:43 +0530

    done

---
+++ 4sum.java
@@ -0,0 +1,24 @@
+class 4sum {
+    public int fourSumCount(int[] nums1, int[] nums2, int[] nums3, int[] nums4) {
+        Map<Integer, Integer> hash = new HashMap<>();
+        for(int i=0;i<nums3.length;i++){
+            for(int j=0;j<nums4.length;j++){
+                int sum = nums3[i]+nums4[j];
+
+                hash.put(sum , hash.getOrDefault(sum,0)+1);
+
+            }
+        }
+        int ans =0;
+        for(int i=0;i<nums1.length;i++){
+            for(int j=0;j<nums2.length;j++){
+                
+
+                ans += hash.getOrDefault(-(nums1[i]+nums2[j]),0);
+
+            }
+        }
+        return ans;
+        
+    }
+}
